1e70e1ad1293b4a5755805a8bdcc5342aa8de93d
artur <artur.oliveirasantiago@gmail.com>
2024-01-08 17:22:00 -0300

    chore: Using `Home` and `Header`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,13 @@
-import Image from 'next/image'
 import React from 'react'
 
-import * as S from '../styles/home.styles'
+import HomeComponent from '../components/Home'
+import Header from '../components/Header'
 
 export default function Home() {
   return (
-    <S.Main>
-      <S.Content>
-        <h1>Em construção</h1>
-        <S.LoadingDots>
-          <span></span>
-          <span></span>
-          <span></span>
-        </S.LoadingDots>
-      </S.Content>
-      <Image
-        src="/assets/icons/in-construction.png"
-        alt="Em construção"
-        width={100}
-        height={100}
-      />
-    </S.Main>
+    <>
+      <Header />
+      <HomeComponent />
+    </>
   )
 }
--- src/styles/home.styles.ts
+++ src/styles/home.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 
 export const Main = styled.main`
   background: #f1f1f1;
@@ -16,35 +16,38 @@ export const Content = styled.div`
   column-gap: 0.1rem;
 `
 
-export const LoadingDots = styled.div`
-  & span {
-    display: inline-block;
-    width: 7px;
-    height: 7px;
-    border-radius: 50%;
-    background: #333333;
-    margin: 5px;
+const bounce = keyframes`
+   to {
+      transform: translateY(5px);
+    }
+`
 
-    animation: bounce 0.6s cubic-bezier(0.6, 0.1, 1, 0.4);
-    animation-iteration-count: infinite;
-    animation-direction: alternate;
-  }
+export const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
 
-  span:nth-child(1) {
+export const Dot = styled.div`
+  width: 7px;
+  height: 7px;
+  border-radius: 50%;
+  background: #333333;
+  margin: 5px;
+
+  animation: ${bounce} 0.6s cubic-bezier(0.6, 0.1, 1, 0.4);
+  animation-iteration-count: infinite;
+  animation-direction: alternate;
+
+  &:nth-child(1) {
     animation-delay: 0.1s;
   }
 
-  span:nth-child(2) {
+  &:nth-child(2) {
     animation-delay: 0.2s;
   }
 
-  span:nth-child(3) {
+  &:nth-child(3) {
     animation-delay: 0.3s;
-  }
-
-  @keyframes bounce {
-    to {
-      transform: translateY(5px);
-    }
   }
 `
