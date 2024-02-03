083dde57d78c1dfab00e49bbd5195bc3cdafb12b
artur <artur.oliveirasantiago@gmail.com>
2024-02-03 20:42:34 -0300

    feature: add `hasScrolledUp` method on `useScroll`

--- src/hooks/useScroll.ts
+++ src/hooks/useScroll.ts
@@ -2,22 +2,29 @@ import { useState, useEffect } from 'react'
 
 const useScroll = () => {
   const [scrollPosition, setScrollPosition] = useState(0)
+  const [lastScrollPosition, setLastScrollPosition] = useState(0)
 
   useEffect(() => {
     const updateScrollPosition = () => {
-      setScrollPosition(window.scrollY)
+      const currentScrollPosition = window.scrollY
+      setLastScrollPosition(scrollPosition) // Atualiza a última posição de rolagem antes de mudar a posição atual
+      setScrollPosition(currentScrollPosition)
     }
 
     window.addEventListener('scroll', updateScrollPosition)
 
     return () => window.removeEventListener('scroll', updateScrollPosition)
-  }, [])
+  }, [scrollPosition])
 
   const isScrollAtValue = (targetValue) => {
     return scrollPosition >= targetValue
   }
 
-  return { scrollPosition, isScrollAtValue }
+  const hasScrolledUp = () => {
+    return lastScrollPosition > scrollPosition
+  }
+
+  return { scrollPosition, isScrollAtValue, hasScrolledUp }
 }
 
 export default useScroll
