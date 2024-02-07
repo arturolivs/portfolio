9cca4fdf2546f7e7f2918040bf4da4e7d6fd3f05
artur <artur.oliveirasantiago@gmail.com>
2024-02-07 01:18:00 -0300

    chore: workaround for firefox css transform update frame problem

--- src/components/RotatingElement/rotatingElement.styles.ts
+++ src/components/RotatingElement/rotatingElement.styles.ts
@@ -2,6 +2,7 @@ import styled, { css } from 'styled-components'
 
 export const RotatingElement = styled.div`
   ${({ xDeg, yDeg }: { xDeg?: number; yDeg?: number }) => css`
+    backface-visibility: hidden; // TO-DO: check firefox update frame problem
     transform: perspective(700px) rotateY(${xDeg}deg) rotateX(${yDeg}deg);
     transition: transform 0.1s linear;
   `}
