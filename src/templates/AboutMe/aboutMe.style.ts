dcde695c06c460e0afabe1a7381d325adbb9c53f
artur <artur.oliveirasantiago@gmail.com>
2024-02-08 17:51:12 -0300

    feature: add text section on `AboutMe` component

--- src/templates/AboutMe/aboutMe.style.ts
+++ src/templates/AboutMe/aboutMe.style.ts
@@ -1,8 +1,34 @@
-import styled, { css } from 'styled-components'
+import styled from 'styled-components'
+import Grid, { GridItem } from '../../components/Grid'
+import { Heading1, Heading2, Paragraph } from '../../components/Text'
 
-export const AboutMeSection = styled.section`
-  ${() => css`
-    width: 100%;
-    height: 100vh;
-  `}
+export const AboutMe = styled(Grid)`
+  width: 100%;
+  height: 100dvh;
 `
+
+export const ImageSection = styled(GridItem)``
+
+export const TextSection = styled(GridItem)`
+  display: flex;
+  flex-direction: column;
+`
+
+export const AboutMeTitle = styled(Heading1)`
+  font-size: 2.5rem;
+  align-self: flex-end;
+  margin-bottom: 4.5rem;
+`
+
+export const Quote = styled(Heading2)`
+  max-width: 24rem;
+  font-size: 1.5rem;
+  margin-bottom: 2rem;
+`
+
+export const AboutMeText = styled(Paragraph)`
+  line-height: 1.6rem;
+  font-size: 1.125rem;
+`
+
+export const TechSection = styled(GridItem)``
--- src/templates/AboutMe/index.tsx
+++ src/templates/AboutMe/index.tsx
@@ -1,7 +1,34 @@
+// aboutMe.tsx
 import React from 'react'
-
 import * as S from './aboutMe.style'
 
-const AboutMeTemplate = () => <S.AboutMeSection />
+const AboutMeTemplate = () => (
+  <S.AboutMe as="section" rows={12} columns={12}>
+    <S.ImageSection row={3} column={2}>
+      IMAGEM
+    </S.ImageSection>
+
+    <S.TextSection row={3} column={8} columnSize={4} rowSize={6}>
+      <S.AboutMeTitle>Sobre mim</S.AboutMeTitle>
+
+      <S.Quote>
+        Criando soluções digitais, transformo suas ideias em realidade
+      </S.Quote>
+
+      <S.AboutMeText>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Nullam eget felis
+        eget nunc. Eu mi bibendum neque egestas congue quisque. Metus aliquam
+        eleifend mi in nulla posuere sollicitudin aliquam ultrices. Duis
+        ultricies lacus sed turpis tincidunt. Risus nullam eget felis eget nunc
+        lobortis mattis. Eu volutpat odio facilisis mauris sit amet massa vitae.
+      </S.AboutMeText>
+    </S.TextSection>
+
+    <S.TechSection row={9} column={8}>
+      TechSection
+    </S.TechSection>
+  </S.AboutMe>
+)
 
 export default AboutMeTemplate
