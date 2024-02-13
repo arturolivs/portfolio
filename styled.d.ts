96edee7a00cf8c982db519f619d228b729bb6c36
artur <artur.oliveirasantiago@gmail.com>
2024-02-13 16:49:15 -0300

    style: add primary_100 on DefaultTheme

--- styled.d.ts
+++ styled.d.ts
@@ -4,6 +4,7 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       primary_500: string
+      primary_100: string
       secondary_500: string
 
       neutral_500: string
