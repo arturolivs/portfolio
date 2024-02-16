00a2d8c619324e021e4bfb5e5ed8495c2c7cbe19
artur <artur.oliveirasantiago@gmail.com>
2024-02-16 18:04:38 -0300

    chore: add `MyProjects` template

---
+++ src/templates/MyProjects/index.tsx
@@ -0,0 +1,9 @@
+// aboutMe.tsx
+import React from 'react'
+import * as S from './myProjects.style'
+
+const MyProjectsTemplate = () => {
+  return <S.MyProjects />
+}
+
+export default MyProjectsTemplate
